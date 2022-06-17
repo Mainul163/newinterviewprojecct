a12c375c1e103869cf9f79964f2198a1d5cad615
Mainul islam <mainulisraf163@gmail.com>
2022-06-18 01:17:52 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import "./App.css";
+import { useForm } from "react-hook-form";
 function App() {
+  const defaultValues = {
+    firstName: "bill",
+    lastName: "luo",
+    email: "bluebill1049@hotmail.com",
+  };
+  let [num, setNum] = useState(0);
+  let incNum = () => {
+    setNum(Number(num) + 1);
+  };
+  let decNum = () => {
+    if (num > 0) {
+      setNum(num - 1);
+    }
+  };
+  let handleChange = (e) => {
+    setNum(e.target.value);
+  };
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => {
+    if (data.firstName == "" || data.gender == "") {
+      data.firstName = defaultValues.firstName;
+      data.num = num;
+      console.log(data);
+    } else {
+      console.log(data);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <p>mainul islam</p>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input {...register("firstName")} name="name" />
+        <select {...register("gender")}>
+          <option value="female">female</option>
+          <option value="male">male</option>
+          <option value="other">other</option>
+        </select>
+        <button class="btn btn-outline-primary" type="button" onClick={decNum}>
+          -
+        </button>
+
+        <input
+          type="text"
+          class="form-control"
+          value={num}
+          onChange={handleChange}
+        />
+
+        <button class="btn btn-outline-primary" type="button" onClick={incNum}>
+          +
+        </button>
+        <input type="submit" />
+      </form>
     </div>
   );
 }
