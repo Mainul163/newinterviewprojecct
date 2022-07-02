0f79d3d63e13ece0858e1025dff2cf42b4e52859
Mainul islam <mainulisraf163@gmail.com>
2022-07-03 00:36:55 +0600

    header component working

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,14 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="fixed w-screen z-50 bg-slate-300 p-6 px-16">
+      {/* destop & tablet */}
+
+      <div className=" hidden w-full h-full  md:flex "></div>
+
+      {/* mobile  */}
+
+      <div className=" flex md:hidden h-full  w-full "></div>
     </div>
   );
 };
