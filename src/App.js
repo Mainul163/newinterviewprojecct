98d47bb24d23d13f341237a8e2bad451affbd515
Mainul islam <mainulisraf163@gmail.com>
2022-08-09 23:40:00 +0600

    push for git hub

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 
 import Header from "./components/Header";
 import { Routes, Route } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 import { CreateContainer, MainContainer } from "./components";
+import axios from "axios";
 function App() {
+  const [state, setState] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios
+        .get("https://jsonplaceholder.typicode.com/users")
+        .then((res) => console.log(res));
+
+      console.log(result.data);
+      setState(result.data);
+    };
+
+    fetchData();
+  }, []);
   return (
     <AnimatePresence>
       <div className="w-screen h-auto flex flex-col bg bg-slate-100">
--- src/context/StateProvider.jsx
+++ src/context/StateProvider.jsx
@@ -9,3 +9,5 @@ export const StateProvider = ({ reducer, initialState, children }) => (
 );
 
 export const useStateValue = () => useContext(StateContext);
+
+//only push for git hub
